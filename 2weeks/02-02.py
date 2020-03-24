285be3d9d32919458d2bda459077bd9b859a56b1
MoonJeongMin <mjm7028@naver.com>
2020-03-24 19:44:22 +0900

    2주차
    2주차

---
+++ 2weeks/02-02.py
@@ -0,0 +1,12 @@
+#input은 무조건 문자열만 입력을 받기 때문에 계산값이 다르다
+
+a=input()
+b=input()
+result=a+b
+print(a,"+",b,"=",result)
+result=a-b
+print(a,"-",b,"=",result)
+result=a*b
+print(a,"*",b,"=",result)
+result=a/b
+print(a,"/",b,"=",result)
---
+++ 2weeks/02-03.py
@@ -0,0 +1,12 @@
+#input은 무조건 문자열만 입력을 받기 때문에 계산값이 다르다
+
+a=int(input("첫 번째 숫자를 입력하시오 : "))
+b=int(input("두 번째 숫자를 입력하시오 : "))
+result=a+b
+print(a,"+",b,"=",result)
+result=a-b
+print(a,"-",b,"=",result)
+result=a*b
+print(a,"*",b,"=",result)
+result=a/b
+print(a,"/",b,"=",result)
